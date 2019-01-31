651dc1fce12777488075fb14d737f84c9d7e4392
Aziox <azioxgaming@hotmail.com>
2019-01-31 15:44:05 +0100

    je crois c'est bon

---
+++ Agenga/Agenga/Controllers/BrokerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Agenga.Models;
+
+namespace Agenga.Controllers
+{
+    public class BrokerController : Controller
+    {
+        public ActionResult addBroker()
+        {
+            return View();
+        }
+        public ActionResult listBrokers()
+        {
+            return View();
+        }
+    }
+}
---
+++ Agenga/Agenga/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agenga.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult addCustomer()
+        {
+            return View();
+        }
+        public ActionResult listCustomers()
+        {
+            return View();
+        }
+    }
+}
